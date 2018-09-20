50de9e373d0f48433504de242c5e868e28600686
Marc <marc.j.bouwman@gmail.com>
2018-09-20 20:51:40 +0200

    Layout improvements

--- StudentMeal/StudentMeal.DataAccess/FakeData/FakeDataRepository.cs
+++ StudentMeal/StudentMeal.DataAccess/FakeData/FakeDataRepository.cs
@@ -6,6 +6,8 @@ using System.Text;
 
 namespace StudentMeal.DataAccess {
     internal class FakeDataRepository : IRepository {
+        private static readonly Random _random = new Random();
+
         private static readonly ICollection<Student> _students = new HashSet<Student>();
 
         private static readonly ICollection<Meal> _meals = new HashSet<Meal>();
@@ -38,10 +40,9 @@ namespace StudentMeal.DataAccess {
                 var today = DateTime.Today;
 
                 var meal1 = new Meal {
-                    Id = 1,
                     DateTime = new DateTime(today.Year, today.Month, today.Day, 18, 10, 0),
-                    Name = "Pannenkoeken",
-                    Description = "Overheerlijke pannenkoeken.",
+                    Name = "Pannenkoeken 🥞",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rutrum convallis luctus. Etiam facilisis placerat lorem eu finibus. Fusce ullamcorper lorem a sapien dignissim, ac facilisis ante congue. Nullam eget nisi sed velit pulvinar lacinia.",
                     Cook = person2,
                     Price = 1.20F,
                     MaxGuests = 3
@@ -50,9 +51,8 @@ namespace StudentMeal.DataAccess {
                 var next = today.AddDays(2);
 
                 var meal2 = new Meal {
-                    Id = 2,
                     DateTime = new DateTime(next.Year, next.Month, next.Day, 17, 55, 0),
-                    Name = "Friet/patat",
+                    Name = "Friet/patat 🍟",
                     Description = "Franse frietjes uit de oven.",
                     Cook = person3,
                     Price = 0.80F,
@@ -62,10 +62,9 @@ namespace StudentMeal.DataAccess {
                 var next2 = today.AddDays(5);
 
                 var meal3 = new Meal {
-                    Id = 3,
                     DateTime = new DateTime(next2.Year, next2.Month, next2.Day, 18, 0, 0),
-                    Name = "Spaghetti",
-                    Description = "Spaghetti",
+                    Name = "Pizza 🍕",
+                    Description = "Pizza",
                     Cook = person3,
                     Price = 2.80F,
                     MaxGuests = 4
@@ -93,9 +92,15 @@ namespace StudentMeal.DataAccess {
             }
         }
 
-        public void AddStudent(Student student) => _students.Add(student);
+        public void AddStudent(Student student) {
+            student.Id = _random.Next(1000);
+            _students.Add(student);
+        }
 
-        public void AddMeal(Meal meal) => _meals.Add(meal);
+        public void AddMeal(Meal meal) {
+            meal.Id = _random.Next(1000);
+            _meals.Add(meal);
+        }
 
         public void AddStudentAsGuestToMeal(Student student, Meal meal) {
             var mealStudent = new MealStudent {
